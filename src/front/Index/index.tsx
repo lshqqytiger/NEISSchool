bcd99a0767a87ef49691e1309593d6dea66f5e6b
베프 <lshqqytiger@naver.com>
2021-06-20 15:38:28 +0900

    20210620

--- src/front/Index/index.tsx
+++ src/front/Index/index.tsx
@@ -1,17 +1,59 @@
 import React from "react";
+import Axios from "axios";
 
 import JJorm from "../JJorm";
 import Bind from "../ReactBootstrap";
 import L from "../@global/Language";
 
-export default class Index extends JJorm<JJWAK.Page.Props<'Index'>>{
-  render():React.ReactNode{
-    return <article>
-      Hello, {this.props.page}
-      <br />
-      {L.render("test")}
-      <img src="/media/images/example-beach.jpg" />
-    </article>;
+type SchoolData = {
+  SCHUL_NM: string;
+};
+type Properties = {};
+type State = {
+  schoolData: SchoolData;
+};
+
+export default class Index extends JJorm<JJWAK.Page.Props<"Index">, State> {
+  constructor(props: JJWAK.Page.Props<"Index">) {
+    super(props);
+    this.state = {
+      schoolData: {
+        SCHUL_NM: "",
+      },
+    };
+  }
+
+  render(): React.ReactNode {
+    return (
+      <article>
+        <div>
+          <span>학교명을 입력하세요.</span>
+          <input id="schoolNameInput" type="text"></input>
+          <button
+            onClick={async () => {
+              this.setState({
+                schoolData: (
+                  await Axios.get(
+                    `/school/info?schoolName=${
+                      (
+                        document.getElementById(
+                          "schoolNameInput"
+                        ) as HTMLInputElement
+                      ).value
+                    }`
+                  )
+                ).data[0],
+              });
+            }}
+          >
+            학교 검색
+          </button>
+        </div>
+        <div id="schoolInfoBox">
+          <span id="SCHUL_NM">{this.state.schoolData.SCHUL_NM}</span>
+        </div>
+      </article>
+    );
   }
 }
 Bind(Index);
