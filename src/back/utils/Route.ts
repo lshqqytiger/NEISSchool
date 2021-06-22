39be00b0f86b352c14cd1b949bfd1f1044f2f2ef
베프 <lshqqytiger@naver.com>
2021-06-22 16:33:45 +0900

    20210622

--- src/back/utils/Route.ts
+++ src/back/utils/Route.ts
@@ -1,10 +1,80 @@
 import Express from "express";
-import Axios from "axios";
+import Axios, { AxiosAdapter } from "axios";
 
 import { SETTINGS } from "./System";
 import { loadLanguages } from "./Language";
 import { PageBuilder } from "./ReactNest";
 import DB from "./Database";
+import { Logger } from "./Logger";
+
+const registerSchoolToDB = async (schoolData: any, req: any, res: any) => {
+  if (
+    (
+      await DB.Manager.query(
+        `SELECT * FROM school WHERE SD_SCHUL_CODE = '${schoolData.SD_SCHUL_CODE}'`
+      )
+    ).length === 0
+  ) {
+    DB.Manager.query(
+      `INSERT INTO school VALUES(
+      '${schoolData.ATPT_OFCDC_SC_CODE}',
+      '${schoolData.ATPT_OFCDC_SC_NM}',
+      '${schoolData.SD_SCHUL_CODE}',
+      '${schoolData.SCHUL_NM}',
+      '${schoolData.ENG_SCHUL_NM}',
+      '${schoolData.SCHUL_KND_SC_NM}',
+      '${schoolData.LCTN_SC_NM}',
+      '${schoolData.JU_ORG_NM}',
+      '${schoolData.FOND_SC_NM}',
+      '${schoolData.ORG_RDNZC}',
+      '${schoolData.ORG_RDNMA}',
+      '${schoolData.ORG_RDNDA}',
+      '${schoolData.ORG_TELNO}',
+      '${schoolData.HMPG_ADRES}',
+      '${schoolData.COEDU_SC_NM}',
+      '${schoolData.HS_SC_NM}',
+      '${schoolData.INDST_SPECL_CCCCL_EXST_YN}',
+      '${schoolData.HS_GNRL_BUSNS_SC_NM}',
+      '${schoolData.SPCLY_PURPS_HS_ORD_NM}',
+      '${schoolData.ENE_BFE_SEHF_SC_NM}',
+      '${schoolData.DGHT_SC_NM}',
+      '${schoolData.FOND_YMD}',
+      '${schoolData.FOAS_MEMRD}',
+      '${schoolData.LOAD_DTM}'
+    );`
+    );
+  } else {
+    DB.Manager.query(
+      `UPDATE school SET
+    ATPT_OFCDC_SC_CODE = '${schoolData.ATPT_OFCDC_SC_CODE}',
+    ATPT_OFCDC_SC_NM = '${schoolData.ATPT_OFCDC_SC_NM}',
+    SD_SCHUL_CODE = '${schoolData.SD_SCHUL_CODE}',
+    SCHUL_NM = '${schoolData.SCHUL_NM}',
+    ENG_SCHUL_NM = '${schoolData.ENG_SCHUL_NM}',
+    SCHUL_KND_SC_NM = '${schoolData.SCHUL_KND_SC_NM}',
+    LCTN_SC_NM = '${schoolData.LCTN_SC_NM}',
+    JU_ORG_NM = '${schoolData.JU_ORG_NM}',
+    FOND_SC_NM = '${schoolData.FOND_SC_NM}',
+    ORG_RDNZC = '${schoolData.ORG_RDNZC}',
+    ORG_RDNMA = '${schoolData.ORG_RDNMA}',
+    ORG_RDNDA = '${schoolData.ORG_RDNDA}',
+    ORG_TELNO = '${schoolData.ORG_TELNO}',
+    HMPG_ADRES = '${schoolData.HMPG_ADRES}',
+    COEDU_SC_NM = '${schoolData.COEDU_SC_NM}',
+    HS_SC_NM = '${schoolData.HS_SC_NM}',
+    INDST_SPECL_CCCCL_EXST_YN = '${schoolData.INDST_SPECL_CCCCL_EXST_YN}',
+    HS_GNRL_BUSNS_SC_NM = '${schoolData.HS_GNRL_BUSNS_SC_NM}',
+    SPCLY_PURPS_HS_ORD_NM = '${schoolData.SPCLY_PURPS_HS_ORD_NM}',
+    ENE_BFE_SEHF_SC_NM = '${schoolData.ENE_BFE_SEHF_SC_NM}',
+    DGHT_SC_NM = '${schoolData.DGHT_SC_NM}',
+    FOND_YMD = '${schoolData.FOND_YMD}',
+    FOAS_MEMRD = '${schoolData.FOAS_MEMRD}',
+    LOAD_DTM = '${schoolData.LOAD_DTM}'
+  WHERE SD_SCHUL_CODE = '${schoolData.SD_SCHUL_CODE}';`
+    );
+  }
+  return;
+};
 
 export default function (App: Express.Application): void {
   App.get("/", PageBuilder("Index"));
@@ -17,85 +87,19 @@ export default function (App: Express.Application): void {
           KEY: SETTINGS["neis"].key,
           Type: "json",
           pIndex: 1,
-          pSize: 1,
+          pSize: 9,
           SCHUL_NM: req.query.schoolName,
         },
       })
-    ).data.schoolInfo[1].row[0];
-    if (
-      (
-        await DB.Manager.query(
-          `SELECT * FROM school WHERE SCHUL_NM = '${req.query.schoolName}'`
-        )
-      ).length === 0
-    ) {
-      DB.Manager.query(
-        `INSERT INTO school VALUES(
-        '${schoolData.ATPT_OFCDC_SC_CODE}',
-        '${schoolData.ATPT_OFCDC_SC_NM}',
-        '${schoolData.SD_SCHUL_CODE}',
-        '${schoolData.SCHUL_NM}',
-        '${schoolData.ENG_SCHUL_NM}',
-        '${schoolData.SCHUL_KND_SC_NM}',
-        '${schoolData.LCTN_SC_NM}',
-        '${schoolData.JU_ORG_NM}',
-        '${schoolData.FOND_SC_NM}',
-        '${schoolData.ORG_RDNZC}',
-        '${schoolData.ORG_RDNMA}',
-        '${schoolData.ORG_RDNDA}',
-        '${schoolData.ORG_TELNO}',
-        '${schoolData.HMPG_ADRES}',
-        '${schoolData.COEDU_SC_NM}',
-        '${schoolData.HS_SC_NM}',
-        '${schoolData.INDST_SPECL_CCCCL_EXST_YN}',
-        '${schoolData.HS_GNRL_BUSNS_SC_NM}',
-        '${schoolData.SPCLY_PURPS_HS_ORD_NM}',
-        '${schoolData.ENE_BFE_SEHF_SC_NM}',
-        '${schoolData.DGHT_SC_NM}',
-        '${schoolData.FOND_YMD}',
-        '${schoolData.FOAS_MEMRD}',
-        '${schoolData.LOAD_DTM}'
-      );`
-      ).then(async () => {
-        const $body = await DB.Manager.query(
-          `SELECT * FROM school WHERE SCHUL_NM = '${schoolData.SCHUL_NM}';`
-        );
-        return res.send($body);
-      });
+    ).data.schoolInfo[1].row;
+    if (schoolData.length >= 2) {
+      for (let i in schoolData) {
+        registerSchoolToDB(schoolData[i], req, res);
+      }
+      return res.send(schoolData);
     } else {
-      DB.Manager.query(
-        `UPDATE school SET
-      ATPT_OFCDC_SC_CODE = '${schoolData.ATPT_OFCDC_SC_CODE}',
-      ATPT_OFCDC_SC_NM = '${schoolData.ATPT_OFCDC_SC_NM}',
-      SD_SCHUL_CODE = '${schoolData.SD_SCHUL_CODE}',
-      SCHUL_NM = '${schoolData.SCHUL_NM}',
-      ENG_SCHUL_NM = '${schoolData.ENG_SCHUL_NM}',
-      SCHUL_KND_SC_NM = '${schoolData.SCHUL_KND_SC_NM}',
-      LCTN_SC_NM = '${schoolData.LCTN_SC_NM}',
-      JU_ORG_NM = '${schoolData.JU_ORG_NM}',
-      FOND_SC_NM = '${schoolData.FOND_SC_NM}',
-      ORG_RDNZC = '${schoolData.ORG_RDNZC}',
-      ORG_RDNMA = '${schoolData.ORG_RDNMA}',
-      ORG_RDNDA = '${schoolData.ORG_RDNDA}',
-      ORG_TELNO = '${schoolData.ORG_TELNO}',
-      HMPG_ADRES = '${schoolData.HMPG_ADRES}',
-      COEDU_SC_NM = '${schoolData.COEDU_SC_NM}',
-      HS_SC_NM = '${schoolData.HS_SC_NM}',
-      INDST_SPECL_CCCCL_EXST_YN = '${schoolData.INDST_SPECL_CCCCL_EXST_YN}',
-      HS_GNRL_BUSNS_SC_NM = '${schoolData.HS_GNRL_BUSNS_SC_NM}',
-      SPCLY_PURPS_HS_ORD_NM = '${schoolData.SPCLY_PURPS_HS_ORD_NM}',
-      ENE_BFE_SEHF_SC_NM = '${schoolData.ENE_BFE_SEHF_SC_NM}',
-      DGHT_SC_NM = '${schoolData.DGHT_SC_NM}',
-      FOND_YMD = '${schoolData.FOND_YMD}',
-      FOAS_MEMRD = '${schoolData.FOAS_MEMRD}',
-      LOAD_DTM = '${schoolData.LOAD_DTM}'
-    WHERE SCHUL_NM = '${req.query.schoolName}';`
-      ).then(async () => {
-        const $body = await DB.Manager.query(
-          `SELECT * FROM school WHERE SCHUL_NM = '${schoolData.SCHUL_NM}';`
-        );
-        return res.send($body);
-      });
+      registerSchoolToDB(schoolData[0], req, res);
+      return res.send([schoolData[0]]);
     }
   });
   App.get("/admin/load-languages", (req, res) => {
--- src/front/Index/index.tsx
+++ src/front/Index/index.tsx
@@ -4,13 +4,26 @@ import Axios from "axios";
 import JJorm from "../JJorm";
 import Bind from "../ReactBootstrap";
 import L from "../@global/Language";
+import { NONAME } from "dns";
 
 type SchoolData = {
+  ATPT_OFCDC_SC_NM: string;
   SCHUL_NM: string;
+  ENG_SCHUL_NM: string;
+  SCHUL_KND_SC_NM: string;
+  FOND_SC_NM: string;
+  ORG_RDNZC: string;
+  ORG_RDNMA: string;
+  ORG_RDNDA: string;
+  ORG_TELNO: string;
+  COEDU_SC_NM: string;
+  HMPG_ADRES: string;
+  FOND_YMD: number;
 };
 type Properties = {};
 type State = {
   schoolData: SchoolData;
+  duplicatedName: Array<any>;
 };
 
 export default class Index extends JJorm<JJWAK.Page.Props<"Index">, State> {
@@ -18,9 +31,52 @@ export default class Index extends JJorm<JJWAK.Page.Props<"Index">, State> {
     super(props);
     this.state = {
       schoolData: {
+        ATPT_OFCDC_SC_NM: "",
         SCHUL_NM: "",
+        ENG_SCHUL_NM: "",
+        SCHUL_KND_SC_NM: "",
+        FOND_SC_NM: "",
+        ORG_RDNZC: "",
+        ORG_RDNMA: "",
+        ORG_RDNDA: "",
+        ORG_TELNO: "",
+        COEDU_SC_NM: "",
+        HMPG_ADRES: "",
+        FOND_YMD: 0,
       },
+      duplicatedName: [],
     };
+  }
+
+  hide(element: HTMLElement | null) {
+    if (element) element.style.display = "none";
+  }
+
+  show(element: HTMLElement | null) {
+    if (element) element.style.display = "block";
+  }
+
+  handleDuplicatedName() {
+    const arr = [];
+    for (let i = 0; i < this.state.duplicatedName.length; i++) {
+      arr.push(
+        <>
+          <br />
+          <button
+            id={`duplicatedName${i}`}
+            onClick={(e) => {
+              this.setState({ schoolData: this.state.duplicatedName[i] });
+              this.hide(e.currentTarget.parentElement);
+            }}
+          >
+            {this.state.duplicatedName[i].SCHUL_NM} (
+            {this.state.duplicatedName[i].ATPT_OFCDC_SC_NM})
+          </button>
+        </>
+      );
+    }
+    console.log(arr);
+    return arr;
   }
 
   render(): React.ReactNode {
@@ -28,29 +84,86 @@ export default class Index extends JJorm<JJWAK.Page.Props<"Index">, State> {
       <article>
         <div>
           <span>학교명을 입력하세요.</span>
+          <br />
           <input id="schoolNameInput" type="text"></input>
           <button
             onClick={async () => {
-              this.setState({
-                schoolData: (
-                  await Axios.get(
-                    `/school/info?schoolName=${
-                      (
-                        document.getElementById(
-                          "schoolNameInput"
-                        ) as HTMLInputElement
-                      ).value
-                    }`
-                  )
-                ).data[0],
-              });
+              const res = (
+                await Axios.get(
+                  `/school/info?schoolName=${
+                    (
+                      document.getElementById(
+                        "schoolNameInput"
+                      ) as HTMLInputElement
+                    ).value
+                  }`
+                )
+              ).data;
+              if (res.length >= 2) {
+                this.setState({ duplicatedName: res });
+                this.show(document.getElementById("selectSchoolDialogBox"));
+              } else {
+                this.setState({
+                  schoolData: res[0],
+                });
+                this.hide(document.getElementById("selectSchoolDialogBox"));
+              }
             }}
           >
             학교 검색
           </button>
         </div>
-        <div id="schoolInfoBox">
-          <span id="SCHUL_NM">{this.state.schoolData.SCHUL_NM}</span>
+        <div
+          id="schoolInfoBox"
+          style={{
+            display: this.state.schoolData.ATPT_OFCDC_SC_NM ? "block" : "none",
+          }}
+        >
+          <span id="ATPT_OFCDC_SC_NM">
+            소속 교육청: {this.state.schoolData.ATPT_OFCDC_SC_NM}
+          </span>
+          <br />
+          <span id="SCHUL_NM">
+            학교명: {this.state.schoolData.SCHUL_NM} (
+            {this.state.schoolData.ENG_SCHUL_NM})
+          </span>
+          <br />
+          <span id="SCHUL_KND_SC_NM">
+            분류: {this.state.schoolData.FOND_SC_NM}{" "}
+            {this.state.schoolData.COEDU_SC_NM}{" "}
+            {this.state.schoolData.SCHUL_KND_SC_NM}
+          </span>
+          <br />
+          <span id="ORG_RDN">
+            소재지: {this.state.schoolData.ORG_RDNMA}{" "}
+            {this.state.schoolData.ORG_RDNDA} ({this.state.schoolData.ORG_RDNZC}
+            )
+          </span>
+          <br />
+          <span id="ORG_TELNO">
+            전화번호: {this.state.schoolData.ORG_TELNO}
+          </span>
+          <br />
+          <span id="HMPG_ADRES">
+            홈페이지:{" "}
+            <a href={this.state.schoolData.HMPG_ADRES}>
+              {this.state.schoolData.HMPG_ADRES}
+            </a>
+          </span>
+          <br />
+          <span id="FOND_YMD">SINCE {this.state.schoolData.FOND_YMD}</span>
+        </div>
+        <div
+          id="selectSchoolDialogBox"
+          style={{
+            display: this.state.duplicatedName[1] ? "display" : "none",
+          }}
+        >
+          <span>
+            학교명 검색 결과 동일한 이름의 학교가 2개 이상 검색되었습니다.
+            하나를 선택해주세요.
+          </span>
+          {this.handleDuplicatedName()}
         </div>
       </article>
     );
